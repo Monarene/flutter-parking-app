b5999c25429ff3f3d89e675cfb2a33e930b16142
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-07-25 22:06:28 +0100

    aded the location model

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,1 +1,12 @@
 // https://maps.googleapis.com/maps/api/place/nearbysearch/
+
+class Location {
+  final double lat;
+  final double lng;
+
+  Location({this.lat, this.lng});
+
+  Location.fromJson(Map<dynamic, dynamic> parsedJson)
+      : lat = parsedJson['lat'],
+        lng = parsedJson['lng'];
+}
