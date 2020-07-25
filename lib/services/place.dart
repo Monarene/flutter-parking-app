cfa4c973bb43bb6ce2940ed6c4bb51f34506760c
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-07-25 23:27:59 +0100

    added place model

---
+++ lib/services/place.dart
@@ -0,0 +1,27 @@
+import 'package:fluter_locator/services/geometry.dart';
+
+class Place {
+  final String name;
+  final double rating;
+  final int userRatingCount;
+  final String vicinity;
+  final Geometry geometry;
+
+  Place(
+      {this.geometry,
+      this.name,
+      this.rating,
+      this.userRatingCount,
+      this.vicinity});
+
+  Place.fromJson(Map<dynamic, dynamic> parsedJson)
+      : name = parsedJson['name'],
+        rating = (parsedJson["rating"] != null)
+            ? parsedJson["rating"].toDouble()
+            : null,
+        vicinity = parsedJson["vicinity"],
+        geometry = Geometry.fromJson(parsedJson["geometry"]),
+        userRatingCount = (parsedJson["user_rating_total"] != null)
+            ? parsedJson["user_rating_total"]
+            : null;
+}
