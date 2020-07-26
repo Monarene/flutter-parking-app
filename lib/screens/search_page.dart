ea9d4704c51f33c7d4d14256de9c62882aea1dcd
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-07-26 22:52:37 +0100

    added a whole lot

--- lib/screens/search_page.dart
+++ lib/screens/search_page.dart
@@ -1,7 +1,10 @@
+import 'package:fluter_locator/services/geolocator_services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../models/place.dart';
 
@@ -10,6 +13,7 @@ class Search extends StatelessWidget {
   Widget build(BuildContext context) {
     final currentPosition = Provider.of<Position>(context);
     final placesProvider = Provider.of<Future<List<Place>>>(context);
+    final geoservice = GeolocatorService();
 
     return FutureProvider(
       create: (context) => placesProvider,
@@ -38,9 +42,80 @@ class Search extends StatelessWidget {
                               child: ListView.builder(
                                   itemCount: places.length,
                                   itemBuilder: (context, index) {
-                                    return Card(
-                                      child: ListTile(
-                                        title: Text(places[index].name),
+                                    return FutureProvider(
+                                      create: (context) =>
+                                          geoservice.getDistance(
+                                              currentPosition.latitude,
+                                              currentPosition.longitude,
+                                              places[index]
+                                                  .geometry
+                                                  .location
+                                                  .lat,
+                                              places[index]
+                                                  .geometry
+                                                  .location
+                                                  .lng),
+                                      child: Card(
+                                        child: ListTile(
+                                          title: Text(places[index].name),
+                                          subtitle: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              SizedBox(
+                                                height: 3.0,
+                                              ),
+                                              (places[index].rating != null)
+                                                  ? Row(
+                                                      children: <Widget>[
+                                                        RatingBarIndicator(
+                                                          rating: places[index]
+                                                              .rating,
+                                                          itemBuilder: (context,
+                                                                  index) =>
+                                                              Icon(
+                                                            Icons.star,
+                                                            color: Colors.amber,
+                                                          ),
+                                                          itemCount: 5,
+                                                          itemSize: 10.0,
+                                                          direction:
+                                                              Axis.horizontal,
+                                                        )
+                                                      ],
+                                                    )
+                                                  : Row(),
+                                              SizedBox(
+                                                height: 5.0,
+                                              ),
+                                              Consumer<double>(
+                                                builder:
+                                                    (context, meters, widget) {
+                                                  return (meters != null)
+                                                      ? Text(
+                                                          "${places[index].vicinity} \u00b7 ${(meters / 1609).round()} mi")
+                                                      : Container();
+                                                },
+                                              )
+                                            ],
+                                          ),
+                                          trailing: IconButton(
+                                            icon: Icon(Icons.directions),
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                            onPressed: () {
+                                              _launchMapsUrl(
+                                                  places[index]
+                                                      .geometry
+                                                      .location
+                                                      .lat,
+                                                  places[index]
+                                                      .geometry
+                                                      .location
+                                                      .lat);
+                                            },
+                                          ),
+                                        ),
                                       ),
                                     );
                                   }),
@@ -57,5 +132,14 @@ class Search extends StatelessWidget {
               ),
       ),
     );
+  }
+
+  void _launchMapsUrl(double lat, double lng) async {
+    final url = "https://www.google.com/maps/search/?api=1&query=$lat,$lng";
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw "Could not launch URL";
+    }
   }
 }
--- lib/services/geolocator_services.dart
+++ lib/services/geolocator_services.dart
@@ -1,10 +1,17 @@
 import 'package:geolocator/geolocator.dart';
 
 class GeolocatorService {
+  var geolocator = Geolocator();
+
   Future<Position> getLocation() async {
-    var geolocator = Geolocator();
     return await geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
         locationPermissionLevel: GeolocationPermission.location);
+  }
+
+  Future<double> getDistance(double startLatitude, double startLongitude,
+      double endLatitude, double endLongitude) async {
+    return geolocator.distanceBetween(
+        startLatitude, startLongitude, endLatitude, endLongitude);
   }
 }
